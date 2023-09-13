71f08d0fd95db60e57cc9d25802ee63892a351ba
khanjancentennial <khanjancentennial@gmail.com>
2023-09-13 12:23:14 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// import React, { useState } from "react";
+// import { View, Text, TextInput,Button, StyleSheet, Switch } from "react-native";
+
+// export default function HelloWorldApp() {
+//   const [value, onChangeText] = 	React.useState('Enter anything');  
+//   const [isDarkMode, setDarkMode] = React.useState(false);
+
+
+//   // Function to toggle the background color between white and dark
+//   const toggleBackgroundColorChange = () => {
+//     setDarkMode((prevIsDarkMode) => !prevIsDarkMode);
+//   };
+
+//   // Task 1 and Task 2
+//   return (  
+//   <View style={[styles.container,{ backgroundColor: isDarkMode ? "black" : "white" }]}>
+//     <TextInput style={styles.edit}
+// 	    onChangeText={text => onChangeText(text)}
+//       clearTextOnFocus={true}
+//       value={value}
+//     />
+//     <Text style={styles.edit}>{value}</Text>
+//     <Button style={styles.button}
+//        onPress={() => onChangeText('')}
+//  	      title="Clear" 
+//        />
+//        <Switch>
+//        value={isDarkMode} onValueChange={toggleBackgroundColorChange}
+//        </Switch>
+//    </View> 
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//        flex: 1,
+//        justifyContent: 'center',
+//        alignItems: 'center'
+//       },
+//   edit: {
+    
+//        fontSize : 30,
+//        color: "black",
+//   },
+//   button: {
+//     marginTop: 20,
+//   }
+// });
+
+
+
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Switch } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+export default function HelloWorldApp() {
+  const [value, onChangeText] = useState('');
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  // Function to toggle the background color between white and dark
+  const toggleBackgroundColorChange = () => {
+    setDarkMode((prevIsDarkMode) => !prevIsDarkMode);
+  };
+
+  // Task 1 and Task 2
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: isDarkMode ? "black" : "white" }]}>
+      <TextInput
+        style={[styles.edit,{color: isDarkMode ? "white" : "black"}]}
+        onChangeText={text => onChangeText(text)}
+        placeholder="Enter Your Name"
+        placeholderTextColor={isDarkMode ? "white" : "black"}
+        clearTextOnFocus={true}
+        value={value}
+      />
+      <Text style={[styles.edit,{color: isDarkMode ? "white" : "black"}]}>{value}</Text>
+      <Button
+        style={styles.button}
+        onPress={() => onChangeText('')}
+        title="Clear"
+      />
+      <Text style={{color: isDarkMode ? "white" : "black",marginBottom: 20,marginTop:20}}>Please press below switch button to enable and diable dark mode</Text>
+      
+      <Switch 
+        value={isDarkMode}
+        onValueChange={toggleBackgroundColorChange}
+      />
     </View>
   );
 }
@@ -13,8 +92,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  edit: {
+    fontSize: 30,
+   
+  },
+  button: {
+    marginTop: 20,
+  }
 });
